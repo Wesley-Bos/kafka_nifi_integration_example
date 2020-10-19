e6d245535a8f75e0f691751b2865009011fe588a
11700775 <boswesley@live.nl>
2020-10-19 07:03:16 +0200

    add producer and consumers

---
+++ consumer_ucll.py
@@ -0,0 +1,12 @@
+from kafka import KafkaConsumer
+
+print("Connecting to consumer ...")
+consumer = KafkaConsumer(
+        'UCLL',
+     bootstrap_servers=['localhost:9092'],
+     auto_offset_reset='latest',
+     enable_auto_commit=True,
+     group_id='my-group')
+
+for message in consumer:
+    print(f"{message.value}")
